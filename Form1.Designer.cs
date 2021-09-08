7c26937b8ac7cee171f5fbe8c595eb2a86515e8c
Aqib <aqib.ahamad21@gmail.com>
2021-09-08 17:42:32 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ImportPriceList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_path = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txt_message = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(29, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select Excel File";
+            // 
+            // txt_path
+            // 
+            this.txt_path.Location = new System.Drawing.Point(101, 14);
+            this.txt_path.Name = "txt_path";
+            this.txt_path.ReadOnly = true;
+            this.txt_path.Size = new System.Drawing.Size(275, 20);
+            this.txt_path.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(297, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Update Price";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt_message
+            // 
+            this.txt_message.Location = new System.Drawing.Point(13, 70);
+            this.txt_message.Multiline = true;
+            this.txt_message.Name = "txt_message";
+            this.txt_message.ReadOnly = true;
+            this.txt_message.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_message.Size = new System.Drawing.Size(398, 170);
+            this.txt_message.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 248);
+            this.Controls.Add(this.txt_message);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txt_path);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Import Price List";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_path;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txt_message;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ImportPriceList
+{
+    public partial class Form1 : Form
+    {
+        const string CONNECTION = @"Data Source=172.16.1.10;Password=2825@Stcc-Sa.Com;User ID=sa;Initial Catalog=Sonic_Data_Test;Persist Security Info=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Excel Workbook|*.xlsx" })
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    txt_path.Text = ofd.FileName;
+                    button2.Enabled = true;
+                }
+            }
+        }
+
+        public DataTable ReadExcel(string fileName)
+        {
+            string conn = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties='Excel 12.0;HDR=YES';";
+            DataTable dtexcel = new DataTable();
+            using(OleDbConnection con = new OleDbConnection(conn))
+            {
+                try
+                {
+                    OleDbDataAdapter oleAdpt = new OleDbDataAdapter("select * from [PriceList$]", con);
+                    oleAdpt.Fill(dtexcel);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message.ToString());
+                }
+                return dtexcel;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                dt = ReadExcel(txt_path.Text);
+                txt_message.Text = "Number of rows: " + dt.Rows.Count.ToString();
+                foreach (DataRow row in dt.Rows)
+                {
+                    UpdatePrice(
+                        row["ItemCode"].ToString(), 
+                        row["UOM"].ToString(), 
+                        Convert.ToDecimal(row["Price"]), 
+                        Convert.ToDecimal(row["MinPrice"]),
+                        Convert.ToDecimal(row["MaxPrice"]),
+                        Convert.ToDecimal(row["CostPrice"])
+                    );
+                }
+
+                button2.Enabled = false;
+                txt_path.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        public void UpdatePrice(string itemCode, string uom, decimal price, decimal minPrice, decimal maxPrice, decimal costPrice)
+        {
+            minPrice = minPrice == -1 ? price : minPrice;
+            maxPrice = maxPrice == -1 ? price : maxPrice;
+
+            SqlConnection conn = new SqlConnection();
+            SqlCommand cmd = new SqlCommand();
+            conn.ConnectionString = CONNECTION;
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "SP_UpdatePriceList";
+
+            cmd.Parameters.AddWithValue("@ItemCode", itemCode);
+            cmd.Parameters.AddWithValue("@UOM", uom);
+            cmd.Parameters.AddWithValue("@Price", price);
+            cmd.Parameters.AddWithValue("@MinPrice", minPrice);
+            cmd.Parameters.AddWithValue("@MaxPrice", maxPrice);
+            cmd.Parameters.AddWithValue("@CostPrice", costPrice);
+            cmd.Parameters.Add("@Message", SqlDbType.VarChar, 4000);
+            cmd.Parameters["@Message"].Direction = ParameterDirection.Output;
+
+            try
+            {
+                conn.Open();
+                int i = cmd.ExecuteNonQuery();
+                string msg = Convert.ToString(cmd.Parameters["@Message"].Value);
+
+                txt_message.AppendText(Environment.NewLine);
+                txt_message.AppendText(msg);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
